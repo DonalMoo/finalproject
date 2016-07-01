0d4c1b30de84a4d7412e222546ee4ca1c23dfb4e
“DonalMoo” <donalmooney87@gmail.com>
2016-07-01 16:19:13 +0100

    change link to mp4

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160630221414) do
+ActiveRecord::Schema.define(version: 20160701144935) do
 
   create_table "campaigns", force: :cascade do |t|
     t.string   "title"
